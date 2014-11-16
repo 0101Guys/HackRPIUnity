40a0c5b7ab6f5a4efb494318fdd2252a03631cf2
Robert Wild <wildr2@rpi.edu>
2014-11-15 20:21:28 -0500

    PropertyObject scripts

---
+++ HackRPIUnity/Assets/Scripts/Box.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Box : PropertyObject 
+{
+    public MonoBehaviour fly_movement;
+    public TextMesh property1_text;
+    
+
+    public override void AddTag(string tag)
+    {
+        if (tag == "Fly")
+        {
+            fly_movement.enabled = true;
+            property1_text.text = "FLY";
+            property1_text.gameObject.SetActive(true);
+        }
+    }
+    public override void RemoveTag(string tag)
+    {
+        if (tag == "Fly")
+        {
+            fly_movement.enabled = false;
+            property1_text.gameObject.SetActive(false);
+        }
+    }
+	
+}
--- HackRPIUnity/Assets/Scripts/PropertyObject.cs
+++ HackRPIUnity/Assets/Scripts/PropertyObject.cs
@@ -4,26 +4,16 @@ using System.Collections.Generic;
 
 public class PropertyObject : MonoBehaviour 
 {
-    public List<string> possible_tags;
-    public List<MonoBehaviour> behaviours;
     public List<string> tags;
 
 
     public virtual void AddTag(string tag)
     {
         tags.Add(tag);
-        int i = possible_tags.IndexOf(tag);
-        MonoBehaviour component = behaviours[i];
-
-        component.enabled = true;
     }
     public virtual void RemoveTag(string tag)
     {
         tags.Remove(tag);
-        int i = possible_tags.IndexOf(tag);
-        MonoBehaviour component = behaviours[i];
-
-        component.enabled = false;
     }
 	
 }
