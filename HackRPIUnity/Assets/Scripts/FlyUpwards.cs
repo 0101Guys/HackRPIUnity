607f0fdb9acbb8ff179a9335d8ce0f32d8285a65
jump4r <morria3@rpi.edu>
2014-11-15 18:56:08 -0600

    Added Script to make box fly upwards.

---
+++ HackRPIUnity/Assets/Scripts/FlyUpwards.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyUpwards : MonoBehaviour {
+
+	private bool canFly = true;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (canFly) {
+			transform.Translate(Vector3.up * Time.deltaTime);
+		}
+	}
+}
