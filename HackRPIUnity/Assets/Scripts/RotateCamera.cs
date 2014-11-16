75d36c154fb95b4493ba411fa32cddd83d346205
jump4r <morria3@rpi.edu>
2014-11-15 20:58:00 -0600

    Added script for rotating the camera.

---
+++ HackRPIUnity/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCamera : MonoBehaviour {
+
+	private float rotSpeed = 20f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Rotate (Vector3.up * Time.deltaTime * rotSpeed);
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)){
+			transform.Rotate (-Vector3.up * Time.deltaTime * rotSpeed);
+		}
+	}
+}
